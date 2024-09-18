3115b17916e72ae2c7b26e17a8adf7689556e90e
Alexandre TAHI <alexandretahi7@gmail.com>
2024-09-18 09:23:01 +0200

    ViewBinding setup

--- app/src/main/java/com/example/mob_tp1/MainActivity.kt
+++ app/src/main/java/com/example/mob_tp1/MainActivity.kt
@@ -1,20 +1,34 @@
 package com.example.mob_tp1
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.mob_tp1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    // Starting point
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // Button initialization
+        binding.btnResult.setOnClickListener {
+            binding.topScreen.text = "${binding.topScreen.text} +1"
         }
     }
 }
